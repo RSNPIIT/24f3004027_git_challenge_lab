5d9c6ae5b0169888d3a0a284656ac50ae1fa563e
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-09-15 14:44:15 +0530

    Hello IITM this is Ramrup I have added 3 python files wherein arithmetic operations have been done as per the scenario4

---
+++ difference.py
@@ -0,0 +1,3 @@
+def subtract(a, b):
+    diff = a - b
+    return diff
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print("Namaste to all professors of IIT Madras")
 print("Feels good to code in such a way that makes it fun")
+
+print("Arithmetic Operation Scenario :-")
+from sum import add
+from difference import subtract
+from product import multiply
+
+print("Python Arithmetic Project")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum:", add(a, b))
+print("Difference:", subtract(a, b))
+print("Product:", multiply(a, b))
