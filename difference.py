282ebf9ac549215431e3922903c15dbaf24749ef
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-09-15 16:24:11 +0530

    Add difference.py for subtraction functionality

--- difference.py
+++ difference.py
@@ -1,3 +1,5 @@
+# This is difference module for PR workflow demonstration
+# I have given these comments for changing so that I can commit the changes in a separate branch
 def subtract(a, b):
     diff = a - b
     return diff
